d28c9e531de0b4ef436ccf4f6d3fcf3669d69b16
Rohit Sabapathi A <rohit@kalpakala.com>
2025-05-04 14:49:16 +0530

    Vidaamuyarchi Part 2

--- linkup-frontend/src/main.jsx
+++ linkup-frontend/src/main.jsx
@@ -5,9 +5,7 @@ import './index.css'
 import { GoogleOAuthProvider } from '@react-oauth/google'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
     <GoogleOAuthProvider clientId="1009859219635-jpmqpp4hvpuj2j85vjn7g700pvpbcb3d.apps.googleusercontent.com">
       <App />
     </GoogleOAuthProvider>
-  </React.StrictMode>
 )
